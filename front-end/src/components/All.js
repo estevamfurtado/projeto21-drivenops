968a5513bda326be13df5af2f490933a90c36510
Estevam <estevamfurtado@gmail.com>
2022-08-20 16:05:44 -0300

    create AllStudents page

---
+++ front-end/src/components/All.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+export default function All() {
+
+    const [students, setStudents] = useState([]);
+
+    console.log('ALL');
+
+    useEffect(async () => {
+        const API_URL = process.env.REACT_APP_BASE_URL;
+        try {
+            const response = await axios.get(`${API_URL}/students`);
+            const students = response.data;
+            if (!students) {
+                alert("Putz! Não há estudantes cadastrados para o sorteio!");
+            } else {
+                setStudents(students);
+            }
+        } catch (error) {
+            alert("Não foi possível realizar o sorteio!");
+        }
+    }, []);
+
+    return <div>
+        {students.length > 0 ? students.map((s, x) => <h1 key={x}>{s.name}</h1>) : "Não existem alunos cadastrados"}
+    </div>
+}
--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,27 +1,19 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import All from "./All";
+import Random from "./Random";
 
-function App() {
-  const [student, setStudent] = useState(null);
-    
-  useEffect(async () => {
-    const API_URL = process.env.REACT_APP_BASE_URL;
-    try {
-      const response = await axios.get(`${API_URL}/students/random`);
-      const student = response.data;
-      if(!student){
-        alert("Putz! Não há estudantes cadastrados para o sorteio!");
-      } else {
-        setStudent(student);
-      }
-    } catch (error) {
-      alert("Não foi possível realizar o sorteio!");
-    }
-  }, []);
-  
-  return (
-    student ? <h1>{student.name}</h1> : "Carregando..."
-  )
+export default function App() {
+
+    console.log('APP!');
+
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Random />} />
+            <Route path="/all" element={<All />} />
+        </Routes>
+    </BrowserRouter>
 }
-
-export default App;
